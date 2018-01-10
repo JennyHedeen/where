c51d88abb3c79ab0c11dd5631e4b17511bdeeaac
John Hedeen <valery.m.trebukh@gmail.com>
2018-01-10 19:26:10 +0200

    Added UserRestController

---
+++ src/main/java/com/myapp/wheretoeat/web/UserRestController.java
@@ -0,0 +1,70 @@
+package com.myapp.wheretoeat.web;
+
+import com.myapp.wheretoeat.model.User;
+import com.myapp.wheretoeat.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+import static com.myapp.wheretoeat.util.ValidationUtil.assureIdConsistent;
+import static com.myapp.wheretoeat.util.ValidationUtil.checkNew;
+
+
+@RestController
+@RequestMapping(UserRestController.REST_URL)
+public class UserRestController {
+
+    static final String REST_URL = "/rest/users";
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<User> getAll() {
+        log.info("getAll");
+        return service.getAll();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public User get(@PathVariable("id") int id) {
+        log.info("get {}", id);
+        return service.get(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createWithLocation(@RequestBody User user) {
+        log.info("create {}", user);
+        checkNew(user);
+        User created =  service.create(user);
+
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable("id") int id) {
+        log.info("delete {}", id);
+        service.delete(id);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody User user, @PathVariable("id") int id) {
+        log.info("update {} with id={}", user, id);
+        assureIdConsistent(user, id);
+        service.update(user);
+    }
+
+}
